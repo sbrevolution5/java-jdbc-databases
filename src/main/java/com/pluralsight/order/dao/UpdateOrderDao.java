5987e3bf7e5482419b2b81562b78749b51c46ac1
Seth A Burleson <Sbrevolution5@aim.com>
2022-04-08 13:41:07 -0400

    part 4

--- src/main/java/com/pluralsight/order/dao/UpdateOrderDao.java
+++ src/main/java/com/pluralsight/order/dao/UpdateOrderDao.java
@@ -6,6 +6,7 @@ import com.pluralsight.order.util.ExceptionHandler;
 
 import java.sql.Connection;
 import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 import java.sql.SQLException;
 
 /**
@@ -31,10 +32,11 @@ public class UpdateOrderDao {
     public int updateOrderStatus(ParamsDto paramsDto) {
         int numberResults = 0;
 
-        try (Connection con = null;
+        try (Connection con = database.getConnection();
              PreparedStatement ps = createPreparedStatement(con, paramsDto)
         ) {
-
+            ResultSet rs = ps.executeQuery();
+            numberResults=rs.getInt(0);
         } catch (SQLException ex) {
             ExceptionHandler.handleException(ex);
         }
@@ -50,7 +52,9 @@ public class UpdateOrderDao {
      * @throws SQLException In case of an error
      */
     private PreparedStatement createPreparedStatement(Connection con, ParamsDto paramsDto) throws SQLException {
-
-        return null;
+        PreparedStatement ps = con.prepareStatement(query);
+        ps.setString(1, paramsDto.getStatus());
+        ps.setLong(2,paramsDto.getOrderId());
+        return ps;
     }
 }
